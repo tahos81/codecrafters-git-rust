cfa3224778f4bd7c4c14dcd77969dd89f2ca3e68
tahos81 <tahirozpala@gmail.com>
2023-05-20 02:34:19 +0300

    Stage 3 Attempt 8

--- src/main.rs
+++ src/main.rs
@@ -45,8 +45,8 @@ fn cat_file(blob_sha: &str) {
 fn hash_object(file_name: &str) {
     let content = fs::read_to_string("./".to_string() + file_name).unwrap();
     let header = format!("blob {}\0", content.len());
-    let store = format!("{}{}", content, header);
-    print!("{}", store);
+    let store = format!("{}{}", header, content);
+    //print!("{}", store);
     let mut hasher = Sha1::new();
     hasher.update(store.as_bytes());
     let res = hasher.finalize();
