78241f769e5a54e5c699135ed42da8e79d904b7b
tahos81 <tahirozpala@gmail.com>
2023-05-20 01:27:35 +0300

    Stage 2 Attempt 2

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,20 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
     if args[1] == "init" {
         fs::create_dir(".git").unwrap();
         fs::create_dir(".git/objects").unwrap();
         fs::create_dir(".git/refs").unwrap();
         fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
         println!("Initialized git directory")
+    } else if args[1] == "cat-file" {
+        let blob_sha = &args[3];
+        let dir_name = &blob_sha[..2];
+        let file_name = &blob_sha[2..];
+        let path = format!("./.git/objects/{}/{}", dir_name, file_name);
+        println!("{}", path);
+        let data = fs::read(path).unwrap();
+        println!("{:?}", data);
     } else {
         println!("unknown command: {}", args[1])
     }
