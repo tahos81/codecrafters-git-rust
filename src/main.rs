8f67946c6223d1f4768510362e8497bcd035eb1b
tahos81 <tahirozpala@gmail.com>
2023-05-19 21:36:04 +0300

    Stage 2 Attempt 1

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+//use flate2;
 use std::env;
 use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
     if args[1] == "init" {
         fs::create_dir(".git").unwrap();
         fs::create_dir(".git/objects").unwrap();
